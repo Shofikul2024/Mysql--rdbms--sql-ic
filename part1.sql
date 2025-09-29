9fd3a0a636e1a04147a55f93caf30e566c38681e
shofikul2024 <islamshofik492@gmail.com>
2025-09-29 11:23:01 +0600

    added inventory bd

--- part1.sql
+++ part1.sql
@@ -1,13 +1,9 @@
-
-
-use inventory_db;
-CREATE TABLE inventory_db (
+ 
+CREATE TABLE suppliers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock INT DEFAULT 0,
+    contact_email VARCHAR(150),
+    phone VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
- 
