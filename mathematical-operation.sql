74fa71a8d1e960d22b5281d0630081ea4b3eee96
shofikul2024 <islamshofik492@gmail.com>
2025-10-08 14:32:53 +0600

    added inventory bd

---
+++ mathematical-operation.sql
@@ -0,0 +1,43 @@
+/*
+
+ set @num1=10;
+set @num2=20;
+set @num3=30;
+select
+@num1+@num2+@num3 as  addition ,
+@num1 - @num2 - @num3 as  subtraction,
+(@num1 +@num2 ) / @num3 as  division,
+@num1*@num2*@num3 as  multiplication,
+@num1 % @num2 as  mudulas,
+POWER(@num1 ,2) as power;
+
+*/
+
+
+
+
+
+
+/*
+
+
+
+-- Lower
+SET @text='We are learning sql';
+
+SELECT LOWER(@text) as text;
+
+SELECT UPPER(@text) as text;
+
+SELECT LENGTH(@text) as text;
+
+SELECT CHAR_LENGTH(@text) as text;
+
+SELECT REVERSE(@text) as text;
+
+
+
+*/
+
+
+
